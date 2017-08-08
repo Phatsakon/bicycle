c43f21165927f5607d54be5d8a28fe6b52301f3d
Phatsakon <patsakom0807687433@hotmail.com>
2017-08-08 10:52:51 +0700

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -1,22 +1,22 @@
 class Bicycle {
   
-    public int cadence =0;
-    public int gear =0;
-    public int speed =1;
+     int cadence =0;
+     int gear =0;
+     int speed =1;
      
-    }
-    public void changCadence(int newValue) {
+    
+     void changCadence(int newValue) {
         cadence = newValue;
     }
         
-    public void changGear(int newValue) {
+     void changGear(int newValue) {
         gear = newValue;
     }
         
-    public void speedUp(int increment) {
+    void speedUp(int increment) {
         speed = speed + increment;
     }
-    public void applyBreak(int decrement) {
+     void applyBreak(int decrement) {
         speed = speed - decrement;
         
 }
