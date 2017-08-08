891c1b682702b8610d04baac7c551843cdf8e27d
Phatsakon <patsakom0807687433@hotmail.com>
2017-08-08 09:37:31 +0700

    ทำ จักรยาน

---
+++ Bicycle.java
@@ -0,0 +1,26 @@
+class Bicycle {
+  
+    public int cadence =0;
+    public int gear =0;
+    public int speed =1;
+     
+    }
+    public void changCadence(int newValue) {
+        cadence = newValue;
+    }
+        
+    public void changGear(int newValue) {
+        gear = newValue;
+    }
+        
+    public void speedUp(int increment) {
+        speed + speed = increment;
+    }
+    public void applyBreak(int decrement) {
+        speed + speed = increment;
+        
+}
+    void printStates (){
+      System.out.println("cadence:"+cadence+"speed:"+speed+"gear:"+gear);
+    }
+}
